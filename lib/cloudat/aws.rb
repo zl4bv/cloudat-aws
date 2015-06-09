1d04e1d73bd25c44f3782a2629ec4a329b27550c
Ben Vidulich <ben@vidulich.co.nz>
2015-06-09 20:59:06 +1200

    fix renamed file

--- lib/cloudat/aws.rb
+++ lib/cloudat/aws.rb
@@ -7,6 +7,6 @@ require 'cloudat/aws/version'
 require 'cloudat/aws/constants'
 require 'cloudat/aws/auto_scaling'
 
-require 'cloudat/resource/aws/autoscale_group_resource'
+require 'cloudat/resource/aws/auto_scaling_group_resource'
 require 'cloudat/resource/aws/cfn_stack_resource'
 require 'cloudat/resource/aws/instance_resource'
