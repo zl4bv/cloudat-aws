58c3d8df361c39e1848ff920386bc8853a8c2dfc
Ben Vidulich <ben@vidulich.co.nz>
2015-06-15 11:25:16 +1200

    Fixed file name for case-sensitive file systems

--- lib/cloudat/aws/auto_scaling.rb
+++ lib/cloudat/aws/auto_scaling.rb
@@ -2,5 +2,5 @@ Aws.add_service(:AutoScaling, {
   api: File.join(Aws::API_DIR, 'autoscaling', '2011-01-01', 'api-2.json'),
   docs: File.join(Aws::API_DIR, 'autoscaling', '2011-01-01', 'docs-2.json'),
   paginators: File.join(Aws::API_DIR, 'autoscaling', '2011-01-01', 'paginators-1.json'),
-  resources: File.join(Cloudat::Aws::API_DIR, 'Autoscaling.resources.json')
+  resources: File.join(Cloudat::Aws::API_DIR, 'autoscaling.resources.json')
 })
